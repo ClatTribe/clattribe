7f71e4025c1c7f81ee67cb65abc2a28fee84ef5d
Saurabh2619 <sharmasaurabh2606@gmail.com>
2025-09-03 14:47:16 +0530

    gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,19 +2,18 @@ import type { Metadata } from "next";
 import "./globals.css";
 // import NextTopLoader from "nextjs-toploader";
 
-// Font imports
 import { Montserrat, Lato } from "next/font/google";
 import Script from "next/script";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
-  weight: ["400", "600", "700"], // headings & bold
+  weight: ["400", "600", "700"],
   variable: "--font-montserrat",
 });
 
 const lato = Lato({
   subsets: ["latin"],
-  weight: ["400", "700"], // body text
+  weight: ["400", "700"],
   variable: "--font-lato",
 });
 
@@ -27,21 +26,28 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className={`${montserrat.variable} ${lato.variable}`}>
       <head>
-        {/* Google Tag (gtag.js) */}
-        <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-2RW5GEFKFR"
-        />
-        <Script id="google-analytics">
+        {/* Google Tag Manager (script) */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
           {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-2RW5GEFKFR');
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-54D2C6NS');
           `}
         </Script>
       </head>
       <body className="flex flex-col min-h-screen font-body">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-54D2C6NS"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         {/* <NextTopLoader color="#024687" /> */}
         <main className="flex-1">{children}</main>
       </body>
