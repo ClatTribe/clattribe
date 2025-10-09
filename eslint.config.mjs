60316e725cdcd8b4cd7006ae78b48f2ed59c8042
Saurabh2619 <sharmasaurabh2606@gmail.com>
2025-10-09 13:07:59 +0530

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,15 +2,26 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
+// Compute __dirname in ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+// FlatCompat allows using legacy ESLint configs
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
 const eslintConfig = [
+  // Extend Next.js recommended rules
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // Custom overrides
+  {
+    rules: {
+      // Allow "any" usage
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
