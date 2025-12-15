d79ed952fb4357a943eda37f4380ca62e9a9f299
vivekdevelope <vivekgujjar853@gmail.com>
2025-12-15 15:24:36 +0530

    icon-text-3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,8 +22,8 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: '/shortlogo.png', sizes: '32x32', type: 'image/png' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/shortlogo.png', sizes: '32x32', type: 'image/png' },
+      { url: '/shortlogo.png', sizes: '16x16', type: 'image/png' },
     ],
     apple: '/shortlogo.png',
     shortcut: '/shortlogo.png',
