00722794c275ad1edfdc45f37032bad70e6c6fa0
ClatTribe <ctribeweb@gmail.com>
2026-04-08 23:52:00 +0530

    Add CLAT GK portal - types

---
+++ src/types/gk-types.ts
@@ -0,0 +1,9 @@
+export interface UserProfile {
+  id: string;
+  email: string;
+  full_name: string;
+  accuracy_rate: number;
+  reading_speed: number;
+  total_quizzes: number;
+  created_at: string;
+}
